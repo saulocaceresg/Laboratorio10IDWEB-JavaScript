ce7adfa49dd8ac911aab5d1662d8f49f26994cf7
saulocaceresg <scaceresg@unsa.edu.pe>
2025-10-24 16:31:00 -0500

    Commit 8: scrip4.js creado y primeros contenidos

--- public-html/scripts/script4.js
+++ public-html/scripts/script4.js
@@ -1,3 +1,13 @@
 // Tabla de multiplicar. El usuario ingresa un número y se muestra su tabla del 1 al 12. Programa iterativo
 
-document.write(`<h3>Ejercicio 3 - Tabla de multiplicar</h3>`)
+document.write(`<h3>Ejercicio 3 - Tabla de multiplicar</h3>`);
+
+// Declaración de variable
+let numero;
+numero = parseInt(prompt("Ingrese un número entero:"));
+
+document.write(`<p></p>`);
+for (let i = 1; i <= 12; i++) {
+    let producto = numero * i;
+    
+}
